194d72370db3505940e0462ba76e6696b7d8be65
Ashik Chandra Das <ashik.math.jnu@gmail.com>
2024-04-07 10:57:30 +0200

    Portfolio 1st version

---
+++ test.js
@@ -0,0 +1,79 @@
+{
+    "certification" : {
+      "0" : {
+          "head" : "Google Advanced Data Analytics Professional",
+          "date" : "March 2024",
+          "link" : "https://www.coursera.org/account/accomplishments/professional-cert/F6XQ49MEKS2G",
+          "courses" : "Foundation of Data Science, Get started with Python, Go Beyond the Numbers: Translate Data into Insights, The Power of Statistics, Regression Analysis: Simplify Complex Data Relationships, The Nuts and Bolts of Machine learning, Google Advanced Data Analytics Capstone"
+      }  
+    },
+    "project" : {
+        "0" : {
+            "link" : "https://www.kaggle.com/code/ashikchandradas/exploring-indoor-plant-diversity/notebook",
+            "linkText" : "explore now 🔗",
+            "heading" : "Exploring Indoor Plant Diversity: Insights from Data Visualization.",
+            "list" : {
+                "0" : "Found insights into the diversity, characteristics, and relationships of different indoor plant species.",
+                "1" : "This comprehensive overview enhanced our understanding of their natural habitats, and provided valuable information for plant enthusiasts and horticulturists."
+            }   
+        },
+        "1" : {
+            "link" : "https://www.kaggle.com/code/ashikchandradas/co2-emission-regression-analysis-and-visualization",
+            "linkText" : "explore now 🔗",
+            "heading" : "CO2 emission: Regression Analysis with Visualization.",
+            "list" : {
+                "0" : "mpact of Engine Size in the Fuel Consumption and CO2 Emission.",
+                "1" : "Predict the CO2 Emission based on Linear Regression model and evaluation of the model.",
+                "2" : "K-means Clustering the level of Fuel Consumption based on different parameters."
+            }   
+        }
+    },
+    "experiences" : {
+        "0" : {
+            "head" : "Student Researcher - CFD Lab, Jagannath University",
+            "active" : "1",
+            "date" : "(Sept 2019 - Nov 2021)",
+            "list" : {
+                "0" : "Research fellowship at Department of Mathematics under Jagannath University Development Project.",
+                "1" : "Focus is on studying the Jeffery-Hamel flow and its correspondence to the Navier-Stokes equation in the field of Computational Fluid Dynamics (CFD).",
+                "2" : "Published three research papers in different journals.",
+                "3" : "Using tools for simulation and analysis: MATLAB, COMSOL, MAPLE, TECPLOT"
+            }
+        },
+        "1" : {
+            "head" : "Data Analyst - Label IT BD",
+            "active" : "0",
+            "date" : "(April 2022 - Current)",
+            "list" : {
+                "0" : "As a remote data analyst, my role is to collect, process, and analyze large, complex, structured and unstructured data sets",
+                "1" : "Interpret website data to inform pricing decisions based on the influence of user age and gender.",
+                "2" : "Optimizing user experience by analyzing browsing equipment and browsers and enabling targeted marketing strategies by analyzing user location data.",
+                "3" : "Produce reports and visualizations of these data that helps decision-making and drive outcomes business intelligence."
+            }
+        }
+    },
+    "paper" : {
+        "0" : {
+            "head" : "Effect of various shaped Al2O3 and TiO2 nanoparticles on water-based MHD nanofluid flow through convergent divergent channels.",
+            "link" : "https://ph02.tci-thaijo.org/index.php/SciTechAsia/article/view/241185",
+            "date" : "2021"
+        },
+        "1" : {
+            "head" : "Analysis of magnetohydrodynamic Jeffery-Hamel flow in a convergent-divergent channel using Cu water nanofluid.",
+            "link" : "https://www.banglajol.info/index.php/JES/article/view/54633",
+            "date" : "2021"
+        },
+        "2" : {
+            "head" : "Study on magnetohydrodynamics Cu-water nanofluid flow with different shapes of nanoparticles in a divergent channel.",
+            "link" : "https://www.banglajol.info/index.php/GANIT/article/view/57573",
+            "date" : "2021"
+        }
+    },
+    "conference" : {
+        "0" : {
+            "head" : "Application of Mathematics in Science and Engineering - Webinar",
+            "place" : "Jagannath University, September 2021",
+            "title" : "Effect of various shaped Al2O3 and TiO2 nanoparticles on water-based MHD nanofluid flow through convergent divergent channels."
+        }
+    }
+}
